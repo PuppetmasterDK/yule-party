c1080c050663d76372172baf2e2d24ef1cbed783
Carsten Lützen <carsten@lutzen.dk>
2015-12-27 08:32:44 +0100

    Moved around some stuff

--- src/boot.php
+++ src/boot.php
@@ -1,7 +1,3 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
-$yule = new \Lutzen\Models\YuleDate(\Carbon\Carbon::now());
-
-echo "Hello: " + $yule->getYulePartyDate()->toDateTimeString() . PHP_EOL;
--- tests/YuleDateTest.php
+++ tests/YuleDateTest.php
@@ -1,7 +1,5 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
 class YuleDateTest extends PHPUnit_Framework_TestCase
 {
 
